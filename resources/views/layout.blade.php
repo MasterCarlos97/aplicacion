fb044fd06026621d89920eca2b0cb0c99b65aaf7
Carlos Alberto Hurtado Dominguez <charlyh2008@hotmail.com>
2019-07-26 12:52:27 -0700

    mas cambios aun

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,24 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
 </head>
-<body class="b-page">
-    {{-- <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-        <a class="navbar-brand" href=" {{ route('home')}} ">
-            <img src="{{asset('/images/descarga.png')}}" width="100px" height="100px" >
-        </a>
-        <div id="my-nav" class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link active" href=" {{ route('home')}} " tabindex="-1" aria-disabled="true">Inicio</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link active" href=" {{ route('contact')}} " tabindex="-1" aria-disabled="true">Quienes somos</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <div>
-            @yield('content')
-    </div> --}}
+<body>
+    
 </body>
 </html>
