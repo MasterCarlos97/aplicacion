361e0061950491574d2dfc91f238d43f1bdd327c
Carlos Alberto Hurtado Dominguez <charlyh2008@hotmail.com>
2019-07-29 09:48:27 -0700

    Cambios

--- resources/views/contact.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-
-@section('title','Quienes somos')
-
-@section('content')
-    <h1>Quienes somos</h1>
-@endsection
--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-
-@section('title','Pagina Principal')
-
-@section('content')
-    <h1>Pagina Principal</h1>
-@endsection
